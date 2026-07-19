0d8f43ca93f60e15c4ee0ff8e4561d6ec4dd9c29
Sergey Panchenko <serj129@gmail.com>
2026-07-19 16:04:58 +0500

    i18n init

--- addon/controllers/portal/home.js
+++ addon/controllers/portal/home.js
@@ -4,12 +4,15 @@ import { inject as service } from '@ember/service';
 export default class PortalHomeController extends Controller {
     @service currentUser;
     @service customerSession;
+    @service intl;
 
     get customerName() {
         return this.customerSession.get('name') || this.currentUser.name;
     }
 
     get accountTypeLabel() {
-        return this.customerSession.accountType === 'vendor' ? 'Company customer workspace' : 'Customer workspace';
+        return this.customerSession.accountType === 'vendor'
+            ? this.intl.t('portal.home.company-workspace')
+            : this.intl.t('portal.home.customer-workspace');
     }
 }
